e34bba36d194a636692bdfb484b71a5e4479ea65
Zignuts Technolab <ztlab111@Zignutss-Mac-mini.local>
2023-01-06 17:11:14 +0530

    all

--- app/src/main/java/com/example/asssignmet_all/navigation.kt
+++ app/src/main/java/com/example/asssignmet_all/navigation.kt
@@ -1,14 +1,30 @@
 package com.example.asssignmet_all
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_navigation.*
 
 class navigation : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
+        
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
+
+        var preference: SharedPreferences =getSharedPreferences("mypres", Context.MODE_PRIVATE)
+        var savestring2 = preference.getString("e_email","email")
         changeFragment(PFragment())
         bottomNavigationView.setOnItemSelectedListener{
             when(it.itemId)
@@ -28,8 +44,121 @@ class navigation : AppCompatActivity() {
         }
 
 
+        fun new()
+        {
+
+            val sharein = Intent()
+            sharein.action = Intent.ACTION_SEND
+            sharein.type = "text/plain"
+            sharein.putExtra(
+                Intent.EXTRA_TEXT,
+                "https://play.google.com/store/apps/details?id= hi"
+            )
+            startActivity(Intent.createChooser(sharein, "Share With App..."))
+        }
+
+        fun new2()
+        {
+            val Email = Intent(Intent.ACTION_SEND)
+            Email.type = "text/email"
+            Email.putExtra(Intent.EXTRA_EMAIL, arrayOf(" "))
+            Email.putExtra(Intent.EXTRA_SUBJECT, "Feedback")
+            Email.putExtra(Intent.EXTRA_TEXT, "Dear ...," + "")
+            startActivity(Email)
+
+        }
+
+        fun new3()
+        {
+
+//            val intent = Intent(this, about::class.java)
+//            startActivity(intent)
+
+            Toast.makeText(this, "Email and Password successfully", Toast.LENGTH_LONG).show()
+
+        }
+
+        fun new4()
+        {
+            Toast.makeText(this, "Email and Password successfully", Toast.LENGTH_LONG).show()
+//            val intent = Intent(this, loginss::class.java)
+//            startActivity(intent)
+//            finish()
+        }
+
+
+
+
+
+        var drew =findViewById<DrawerLayout>(R.id.drawerLayout)
+
+        toggle= ActionBarDrawerToggle(this, drew, R.string.open,R.string.close)
+        drew.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+
+
+
+
+        var views=findViewById<NavigationView>(R.id.navview)
+
+
+
+        views.setNavigationItemSelectedListener {
+
+
+            when(it.itemId)
+            {
+                R.id.mitItem1->new()
+
+                R.id.mitItem2-> new2()
+
+                R.id.mitItem3-> new3()
+
+                R.id.mitItem4 -> new4()
+            }
+            true
+
+
+            true
+        }
+
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.login,menu)
+
+
+
+
+        return true
+    }
+
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item))
+        {
+
+
+            return true
+        }
+
+
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
+
+
 
     private fun changeFragment(fragment : Fragment)
     {
